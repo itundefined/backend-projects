470f61dd73ac935b54ebf307122c49e35c6f314a
Tushar Sharma <tusharsharmaek@gmail.com>
2022-08-25 17:36:24 +0530

    Update allposts.js

--- Node Js backend, everything to learn/Routes/allposts.js
+++ Node Js backend, everything to learn/Routes/allposts.js
@@ -1,14 +1,9 @@
 const express = require('express'); 
 const router = express.Router();
 module.exports = router;
+let allposts = require("../model/localStorage")
 
-const allposts = [{ 'Arthur': "it is me arthur morgan and i know i am dangerous"}, {'dutch':"it is me Dutch and i know i am dangerous"} ];
 
 router.get('/', (req, res) => {
     res.send(allposts)
-})
-
-
-router.post('/', (req, res) => {
-    
 })
