68f86ae9050eaa8c57d5d58b257db23af80d8de8
Tushar Sharma <tusharsharmaek@gmail.com>
2022-08-25 17:35:47 +0530

    Create localStorage.js

---
+++ Node Js backend, everything to learn/model/localStorage.js
@@ -0,0 +1,3 @@
+const allposts = [{"projects":{'Youtube': "https://itundefined.netlify.app/youtubeclone/html/", 'Todo List':"https://itundefined.netlify.app/advancetodolist/todo"}, "socialMedia":{"github":"https://github.com/itundefined", "twitter":"https://twitter.com/itundefine"}}];
+
+module.exports = allposts;
