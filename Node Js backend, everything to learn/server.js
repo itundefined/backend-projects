def33b3b46ca6a364854392a7824720d4f9ce199
Tushar Sharma <tusharsharmaek@gmail.com>
2022-08-25 17:36:47 +0530

    Update server.js

--- Node Js backend, everything to learn/server.js
+++ Node Js backend, everything to learn/server.js
@@ -1,44 +1,16 @@
-// Create a server,                 [DONE]
-// Routes,                          [DONE]
-// Authentication,                  [DONE]
-// User Role to access the page     [DONE]
-// Then keep the user saved with json web token and save web token in localstorage and secret key to server .env file's variable
-
 const express = require('express');
 const app = express();
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 3000);
 
 app.use(express.json());
+
 
 const createAccountRouter = require('./Routes/create-account')
 const loginRouter = require('./Routes/login');
 const allpostsRouter = require('./Routes/allposts');
 const subscribersRouter = require('./Routes/subscribers');
 
-
-
+app.use("/", allpostsRouter);
 app.use("/create-account", createAccountRouter);
 app.use('/login', loginRouter);
-app.use("/allposts", allpostsRouter);
-app.use("/subscribers", subscribersRouter);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use("/admins", subscribersRouter);
